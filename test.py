e00b46b2917b4fac470416f25cc3734ffeb37ae8
LeahJKH <LeahJb06.kodehode@gmail.com>
2025-01-29 14:06:24 +0100

    komenterer

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 # Console.log eller Console.WriteLine
 # print("goodbye world")
+
+#her importer vi decimal så vi kan hente ut desimal tall senere
 import decimal
 
-num = 2
+# num = 2
+
 def summer(num1, num2):
     print( num1 + num2)
 
 def Num2Creator(num1):
     print("give second number")
-    NummerTo = decimal.Decimal(input())
+    NummerTo = decimal.Decimal(input()) # her bruker vi input så vi kan skrive selv i consollen
     summer(num1, NummerTo)
 
 
 def num1Creator(): 
     print("give first number")
-    NummerEn = decimal.Decimal(input())
-    Num2Creator(NummerEn)
+    NummerEn = decimal.Decimal(input()) # her bruker vi input så vi kan skrive selv i consollen
+    Num2Creator(NummerEn) # vi sender tallet videre i neste function som et argument
 
 
 num1Creator()
