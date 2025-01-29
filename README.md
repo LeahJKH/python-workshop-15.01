6c390764f8be4b0377ecb041e1a7b4d03f8064b3
LeahJKH <Leahb06.kodehode@gmail.com>
2025-01-29 14:06:24 +0100

    komenterer

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Hei hei gjennom denne workshoppen gikk vi gjennom litt forskjellig.
 
+vi satte oss for hva syntax rundt python er og at python er et indentation language
+
+python er mest brukt til scripting og er et veldig flexibelt språk med pakker til å løse di fleste problemer
+
 ## oppgave 1
 som frivlig oppgave vil jeg at dere prøver og lage en egen kalkulator som tar in operators som 
 - "+"
--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
+# i stedet for og bygge en fil for og  hente informasjon fra nettet bruker vi requests som er en ferdig bygd en
 import requests
 import json
 
-Url = "https://miku-api.vercel.app/vocaloids/"
+Url = "https://miku-api.vercel.app/vocaloids/" # her putter vi inn url vi skal hente fra
 Response = requests.get(Url)
-JsonObj = Response.json()
+JsonObj = Response.json() # vi gir koden beskjed at dette er en json fil
 
-with open("vocaloids.json", "w") as file:
-    json.dump(JsonObj, file, indent=1)
+with open("vocaloids.json", "w") as file: #her gjør vi så vi lager en ny fil "w" står for write
+    json.dump(JsonObj, file, indent=1) #her passer vi på at filen ser ut og ikke ser ut som knot
